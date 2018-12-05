954ad630097d5d5c5e1d957e5820dc8e87fd2b6d
iog320 <2713014632@qq.com>
2018-12-06 01:18:15 +0800

    add route

---
+++ service/planets.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/unrolled/render"
+)
+
+//handle a request with method GET and path "/api/".
+func planetsHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct {
+			Films     string `json:"films"`
+			People    string `json:"people"`
+			Planets   string `json:"planets"`
+			Species   string `json:"species"`
+			Starships string `json:"starships"`
+			Vehicles  string `json:"vehicles"`
+		}{Films: "https://swapi.co/api/films/",
+			People:    "https://swapi.co/api/people/",
+			Planets:   "https://swapi.co/api/planets/",
+			Species:   "https://swapi.co/api/species/",
+			Starships: "https://swapi.co/api/starships/",
+			Vehicles:  "https://swapi.co/api/vehicles/"})
+	}
+}
+
+func getPlanetsById(w http.ResponseWriter, req *http.Request) {
+
+}

dd83d71ea9757dd230961d211dba50cc79eee447
ddghost <544011312@qq.com>
2018-12-05 23:25:29 +0800

    db

---
+++ database/database/database.go
@@ -0,0 +1,110 @@
+package database
+
+import (
+	"github.com/boltdb/bolt"
+	"log"
+	"os"
+	"time"
+
+)
+
+var dbName = "test.db"
+
+func SetDbName(str string){
+	dbName = str
+}
+
+
+func Init(){
+	db, err := bolt.Open(dbName, 0666, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil{
+		log.Fatal(err)
+		return 
+	}
+	if err := db.Update(func(tx *bolt.Tx) error {
+		tx.CreateBucket([]byte("film") )
+		tx.CreateBucket([]byte("person") )
+		tx.CreateBucket([]byte("planet") )
+		tx.CreateBucket([]byte("species") )
+		tx.CreateBucket([]byte("starship") )
+		tx.CreateBucket([]byte("vehicle") )
+		
+        return nil
+    }); err != nil {
+        log.Fatal(err)
+	}
+	if err := db.Close(); err != nil {
+        log.Fatal(err)
+	}
+}
+
+func DeleteDB(){
+	os.Remove(dbName)
+}
+
+func Update(bucketName []byte ,key []byte ,value []byte){
+	db, err := bolt.Open(dbName, 0666, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil{
+		log.Fatal(err)
+		return 
+	}
+	if err := db.Update(func(tx *bolt.Tx) error {
+        // Create a bucket.
+        if err := tx.Bucket(bucketName).Put(key, value); err != nil {
+            return err
+		}
+        return nil
+    }); err != nil {
+        log.Fatal(err)
+    }
+	if err := db.Close(); err != nil {
+        log.Fatal(err)
+	}
+}
+
+
+func GetValue(bucketName []byte ,key []byte )(string){
+	db, err := bolt.Open(dbName, 0666, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil{
+		log.Fatal(err)
+	}
+	var result []byte
+	if err := db.View(func(tx *bolt.Tx) error {
+		//value = tx.Bucket([]byte(bucketName)).Get(key)
+		byteLen := len(tx.Bucket([]byte(bucketName)).Get(key) )
+		result = make([]byte, byteLen)
+		copy(result[:] , tx.Bucket([]byte(bucketName)).Get(key)[:] )
+		return nil
+    }); err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Close(); err != nil {
+        log.Fatal(err)
+	}
+	
+	return string(result) 
+}
+
+//debug
+func CheckBucket(bucketName []byte ){
+	db, err := bolt.Open(dbName, 0666, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil{
+		log.Fatal(err)
+		return 
+	}
+
+	if err := db.View(func(tx *bolt.Tx) error {
+		b  := tx.Bucket([]byte(bucketName))
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			log.Printf("key=%s, value=%s\n", k, v)
+		}
+        return nil
+    }); err != nil {
+        log.Fatal(err)
+	}
+
+	if err := db.Close(); err != nil {
+        log.Fatal(err)
+    }
+}
---
+++ database/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"encoding/json"
+	
+	/*"strings"*/
+	"strconv"
+	"log"
+	"github.com/peterhellberg/database/swapi"
+	"github.com/peterhellberg/database/database"
+)
+
+
+
+
+func main() {
+	reSetDB := true
+	database.SetDbName("./database/test.db")
+	if(reSetDB){
+		reSetDatabase()
+	}
+
+	//example
+	//1-7
+	//value := database.GetValue([]byte("film") , []byte("1"))
+	//1-17
+	//value := database.GetValue([]byte("person") , []byte("1"))
+	//1-61
+	//value := database.GetValue([]byte("planet") , []byte("1"))
+	//1-37
+	//value := database.GetValue([]byte("species") , []byte("1"))
+	//??
+	//value := database.GetValue([]byte("starship") , []byte("1"))
+	//value := database.GetValue([]byte("vehicle") , []byte("1"))
+	//log.Println( value )
+}
+
+func reSetDatabase(){
+	//database.DeleteDB()
+	//database.Init()
+	/*findFilm()
+	findPerson()
+	findPlanet()
+	findSpecies()*/
+	
+	findVehicle()
+	findStarship()
+}
+
+
+func findFilm(){
+	c := swapi.DefaultClient
+	for index := 1; ; index++ {
+		jsonStr := dump(c.Film(index) )
+		indexStr := strconv.Itoa(index)
+		if len(database.GetValue([]byte("film") , []byte(indexStr))) == 0{
+			if !putIntoDb([]byte("film") , []byte(indexStr) ,  jsonStr){
+				break
+			}
+		}
+	}
+}
+
+func findPerson(){
+	c := swapi.DefaultClient
+	for index := 1; ; index++ {
+		jsonStr := dump(c.Person(index) )
+		indexStr := strconv.Itoa(index)
+		if len(database.GetValue([]byte("person") , []byte(indexStr))) == 0{
+			if(!putIntoDb([]byte("person") , []byte(indexStr) , jsonStr) ){
+				break
+			}
+		}
+	}
+}
+
+func findPlanet(){
+	c := swapi.DefaultClient
+	for index := 1; ; index++ {
+		jsonStr := dump(c.Planet(index) )
+		indexStr := strconv.Itoa(index)
+		if len(database.GetValue([]byte("planet") , []byte(indexStr))) == 0{
+			if(!putIntoDb([]byte("planet") , []byte(indexStr) , jsonStr) ){
+				break
+			}
+		}
+	}
+}
+
+func findSpecies(){
+	c := swapi.DefaultClient
+	for index := 1; ; index++ {
+		jsonStr := dump(c.Species(index) )
+		indexStr := strconv.Itoa(index)
+		if len(database.GetValue([]byte("species") , []byte(indexStr))) == 0{
+			if(!putIntoDb([]byte("species") , []byte(indexStr) , jsonStr) ){
+				break
+			}
+		}
+	}
+}
+
+func findStarship(){
+	c := swapi.DefaultClient
+	for index := 1; ; index++ {
+		jsonStr := dump(c.Starship(index) )
+		indexStr := strconv.Itoa(index)
+		if len(database.GetValue([]byte("starship") , []byte(indexStr))) == 0{
+			if(!putIntoDb([]byte("starship") , []byte(indexStr) , jsonStr) ){
+				break
+			}
+		}
+	}
+}
+
+func findVehicle(){
+	c := swapi.DefaultClient
+	for index := 1; ; index++ {
+		jsonStr := dump(c.Vehicle(index) )
+		indexStr := strconv.Itoa(index)
+		if(!putIntoDb([]byte("vehicle") , []byte(indexStr) , jsonStr) ){
+			break
+		}
+	}
+}
+
+
+
+
+
+func dump(data interface{}, err error)([]byte) {
+	jsonStr, err := json.MarshalIndent(data, "", "  ")
+	return jsonStr
+}
+
+func putIntoDb(bucketName []byte ,  index []byte, jsonStr []byte )(bool){
+	stb := &swapi.Film{}
+	err := json.Unmarshal(jsonStr, &stb)
+	if(err != nil){
+		log.Fatal(err)
+		return false
+	}else if len(stb.URL) == 0{
+		return false
+	}
+	log.Printf("solve %s/%s" ,  bucketName , index)
+	database.Update(bucketName ,index , jsonStr)
+ 	return true
+	
+}
